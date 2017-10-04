411bb861c873ea988c1fd95cfecfbb0793e66d85
Ian Otenti <iotenti@gmail.com>
2017-10-04 11:45:26 -0400

    done

--- LAB1.php
+++ LAB1.php
@@ -1,30 +1,29 @@
 <?php
 $table = "<table>\n"; //empty table var
-//$color = A3BC34;
-$color = array();
-for($i = 0; $i < 100; $i++){
+$color = array();//make array called color
+for($i = 0; $i < 100; $i++){//loop through to make a bunch of random colors
 
-    $r1 = dechex(mt_rand(0, 15));
+    $r1 = dechex(mt_rand(0, 15));//gets random num 0-15 then converts it to hex
     $r2 = dechex(mt_rand(0, 15));
     $g1 = dechex(mt_rand(0, 15));
     $g2 = dechex(mt_rand(0, 15));
     $b1 = dechex(mt_rand(0, 15));
     $b2 = dechex(mt_rand(0, 15));
 
-    $color[$i] = $r1 .= $r2 .= $g1 .= $g2 .= $b1 .= $b2;
+    $color[$i] = $r1 .= $r2 .= $g1 .= $g2 .= $b1 .= $b2; //concatenate 6 different hex digits to one var and stores it in the array.
 }
-$counter = 0;
-for($rows = 1; $rows <= 10; $rows++){
-    $table .= "\t<tr>";
-    for($cols = 1; $cols <= 10; $cols++) {
+$counter = 0; //counter to change index of array
+for($rows = 1; $rows <= 10; $rows++){ //makes 10 rows
+    $table .= "\t<tr>"; //open tr tag
+    for($cols = 1; $cols <= 10; $cols++) {//makes 10 columns
         $table .= "<td style='background-color:#$color[$counter];'>" . $color[$counter] . "<br />" . "<span style='color:#ffffff;'>" . $color[$counter] . "</span>" . "</td>";
-        $counter++;
+        $counter++; //increments counter
     }
 
-    $table .= "</tr>\n";
+    $table .= "</tr>\n"; //close tr tag
 }
 
-$table .="</table>";
+$table .="</table>"; //close table tag
 ?>
 
 <!DOCTYPE html>
