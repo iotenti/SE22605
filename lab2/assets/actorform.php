db945bac452e3f9e3f44463c127e140d2c9cb28b
Ian Otenti <iotenti@gmail.com>
2017-10-18 11:02:09 -0400

    2

--- lab2/assets/actorform.php
+++ lab2/assets/actorform.php
@@ -5,3 +5,13 @@
  * Date: 10/18/2017
  * Time: 10:24 AM
  */
+?>
+<form method ="post" action="#">
+    <table>
+        <tr><td>First Name:</td><td> <input type="text" name="fName" value="" /></td></tr>
+        <tr><td>Last Name:</td><td> <input type="text" name="Lname" value="" /></td></tr>
+        <tr><td>Date of birth:</td><td> <input type="text" name="dob" value="" /></td></tr>
+        <tr><td>Height:</td><td> <input type="text" name="height" value="" /></td></tr>
+        <tr><td><input type="submit" id="btn" name="action" value="Submit" /></td></tr>
+    </table>
+</form>
--- lab2/assets/actors.php
+++ lab2/assets/actors.php
@@ -5,3 +5,21 @@
  * Date: 10/18/2017
  * Time: 10:24 AM
  */
+function getActorsAsTable($db){
+    try{
+        $sql = "SELECT * FROM actors";
+        $sql = $db->prepare($sql);
+        $sql->execute();
+        $dogs = $sql->fetchALL(PDO::FETCH_ASSOC);
+        if($sql->rowCount() > 0){
+            $table = "<table>" . PHP_EOL;
+            foreach($actors as $actor){
+                $table .= "<tr><td>" . $actor['fName'] . "</td></tr>";
+                $table .= "<tr><td>" . $actor['lName'] . "</td></tr>";
+                $table .= "<tr><td>" . $actor['dob'] . "</td></tr>";
+                $table .= "<tr><td>" . $actor['height'] . "</td></tr>";
+
+            }
+        }
+    }
+}
