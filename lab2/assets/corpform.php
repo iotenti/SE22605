ca6cc73b43fdd423e6bcb363cea3ead456d485a9
Ian Otenti <iotenti@gmail.com>
2017-10-23 08:18:20 -0400

    fixed form, think i'll have to reado once demo is done

--- lab2/assets/corpform.php
+++ lab2/assets/corpform.php
@@ -10,11 +10,17 @@
     <table>
         <tr><td>Corporation Name:</td><td> <input type="text" name="corp" value="" /></td></tr><!-- text fields for data -->
         <tr><td>Date added:</td><td> <input type="text" name="incorp_dt" value="" /></td></tr>
-        <tr><td>email:</td><td> <input type="text" name="email" value="" /></td></tr>
-        <tr><td>zipcode:</td><td> <input type="text" name="zipcode" value="" /></td></tr>
-        <tr><td>owner:</td><td> <input type="text" name="owner" value="" /></td></tr>
-        <tr><td>phone:</td><td> <input type="text" name="phone" value="" /></td></tr>
-        <tr><td><input type="submit" id="btn" name="action" value="Add" /></td></tr><!-- button used to add. Connects to add switch in lab2.php -->
+        <tr><td>Email:</td><td> <input type="text" name="email" value="" /></td></tr>
+        <tr><td>Zipcode:</td><td> <input type="text" name="zipcode" value="" /></td></tr>
+        <tr><td>Owner:</td><td> <input type="text" name="owner" value="" /></td></tr>
+        <tr><td>Phone:</td><td> <input type="text" name="phone" value="" /></td></tr>
+    </table>
+    <br />
+    <table>
+        <tr><td><input type="submit" id="btn" name="action" value="Add" /></td><!-- button used to add. Connects to add switch in lab2.php -->
+        <td><input type="submit" id="btn" name="action" value="Read" /></td>
+        <td><input type="submit" id="btn" name="action" value="Update" /></td>
+        <td><input type="submit" id="btn" name="action" value="Delete" /></td></tr>
     </table>
     <br /><br />
 </form>
