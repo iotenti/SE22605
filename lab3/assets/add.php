299048998718ee0c7c0649c678eceb02e1a24bd8
Ian Otenti <iotenti@gmail.com>
2017-10-24 17:47:00 -0400

    ADD page Working

--- lab3/assets/add.php
+++ lab3/assets/add.php
@@ -3,5 +3,39 @@
  * Created by PhpStorm.
  * User: iotenti
  * Date: 10/24/2017
- * Time: 5:36 PM
+ * Time: 3:20 PM
  */
+
+require_once("dbconn.php"); //require this file or fatal error.
+require_once("corps.php");
+include_once("header.php"); //include header once.
+$button = "Add";
+require_once("corpform.php");
+
+$db = dbConn(); // run function that connects to the db and store that connection in a var called db.
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT) ?? null;
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ?? filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) ?? "";
+$corp = filter_input(INPUT_POST, 'corp', FILTER_SANITIZE_STRING) ?? "";
+$incorp_dt = filter_input(INPUT_POST, 'incorp_dt', FILTER_SANITIZE_STRING) ?? "";
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING) ?? "";
+$zipcode = filter_input(INPUT_POST, 'zipcode', FILTER_SANITIZE_STRING) ?? "";
+$owner = filter_input(INPUT_POST, 'owner', FILTER_SANITIZE_STRING) ?? "";
+$phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING) ?? "";
+?>
+
+<?php
+if ($action == "Add"){ //switch, if the button has a value of "Add" then run the addActor function and pass it the information.
+    addCorp($db, $corp, $incorp_dt, $email, $zipcode, $owner, $phone);
+    $button = "Add";
+}
+
+?>
+
+    ?>
+    <a href='../index.php'>Home</a>
+    <form method="post" action="#">
+        <input type="submit" id="btn" name="action" value="Add A Record" />
+    </form>
+<?php
+include_once("footer.php"); //call the footer once.
+?>
--- lab3/index.php
+++ lab3/index.php
@@ -6,6 +6,7 @@ include_once("assets/header.php"); //include header once.
 $db = dbConn(); // run function that connects to the db and store that connection in a var called db.
 
 //check to see if these vars have anything in them, and if not, make them equal to an empty string
+/*
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ?? filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) ?? "";
 $corp = filter_input(INPUT_POST, 'corp', FILTER_SANITIZE_STRING) ?? "";
 $incorp_dt = filter_input(INPUT_POST, 'incorp_dt', FILTER_SANITIZE_STRING) ?? "";
@@ -16,7 +17,8 @@ $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING) ?? "";
 
 
 $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT) ?? null;
-
+*/
+/*
 switch ($action){ //switch, if the button has a value of "Add" then run the addActor function and pass it the information.
     case "Add A Record":
         include_once("assets/corpform.php");//this has the meat of the html page. The form being filled out. Called once.
@@ -40,11 +42,14 @@ switch ($action){ //switch, if the button has a value of "Add" then run the addA
         echo $button;
         break;
 }
+*/
 ?>
 <h1>Corporation Name:</h1>
+    <a href='assets/add.php'>Add</a>
 <?php
 echo getCorpName($db);
 ?>
+
 <form method="post" action="#">
     <input type="submit" id="btn" name="action" value="Add A Record" />
 </form>
